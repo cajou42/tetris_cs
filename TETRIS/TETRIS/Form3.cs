54efbf0fd439125c239921486d439da4832161dc
LMERLAUD <louis.merlaud@ynov.com>
2022-02-03 16:45:55 +0100

    music problem

--- TETRIS/TETRIS/Form3.cs
+++ TETRIS/TETRIS/Form3.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,9 @@ namespace TETRIS
         public Form3()
         {
             InitializeComponent();
+            //MyProject.Computer.Audio.Play(test.My.Resources.TAMUSIQUEICI, AudioPlayMode.Background);
+            SoundPlayer NOST = new SoundPlayer(@"c:\Users\louis\tetris_cs\music\Tetris_theme.ogg);
+            NOST.Play();
         }
 
         private void insideOption_Click(object sender, EventArgs e)
